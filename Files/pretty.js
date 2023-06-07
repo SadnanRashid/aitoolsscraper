067afdb934b7a52c73157bac657149aead2ce2fe
SadnanRashid <sadnan.rashid.07@gmail.com>
2023-06-07 17:22:17 +0600

    pretty

--- Files/pretty.js
+++ Files/pretty.js
@@ -6,25 +6,25 @@ const {
 } = require("../file_write");
 
 for (let i = 0; i < data.length; i++) {
+  data[i].tags.push(data[i].category.toLowerCase());
+}
+
+for (let i = 0; i < data.length; i++) {
   const arr = [];
   arr.push(data[i].category.toLowerCase());
   data[i].category = arr;
 }
 
-// for (let i = 0; i < data.length; i++) {
-//   data[i].tags.push(data[i].category.toLowerCase());
-// }
-
-// for (let i = 0; i < data.length; i++) {
-//   if (data[i].tags[0] === "free") {
-//     data[i].payment = "free";
-//   } else if (data[i].tags[0] === "paid") {
-//     data[i].payment = "paid";
-//   } else if (data[i].tags[0] === "freemium") {
-//     data[i].payment = "freemium";
-//   } else {
-//     data[i].payment = "freemium";
-//   }
-// }
+for (let i = 0; i < data.length; i++) {
+  if (data[i].tags[0] === "free") {
+    data[i].payment = "free";
+  } else if (data[i].tags[0] === "paid") {
+    data[i].payment = "paid";
+  } else if (data[i].tags[0] === "freemium") {
+    data[i].payment = "freemium";
+  } else {
+    data[i].payment = "freemium";
+  }
+}
 
 writeToFileArray(data, "AiValleyPretty.json");
